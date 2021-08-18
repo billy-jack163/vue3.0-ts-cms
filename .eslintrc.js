043816585b821a80d9bb41cf3e06d02d14edd711
余顺鹏 <8572225+ysp--zyy@user.noreply.gitee.com>
2021-08-18 17:11:41 +0800

    vue3.0-ts-cms

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+    // 因为vue-cli本身配置了相关prttier，但是我们也配置了相关内容，因此为了使配置文件生效，因此要继承这个插件
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
